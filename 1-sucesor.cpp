3a7b9c35fed3dce2504db40d2c6a210dadca398a
Gr1m <74679791+gr1m2@users.noreply.github.com>
2023-01-26 17:37:02 +0100

    Add files via upload

---
+++ 1-sucesor.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std; //Para poder usar operadores como "cin" o "cout"
+
+int number; //Creación de la variable en la cual se almacena el número
+
+int main(){ //Variable donde estara el codigo ejecutado
+    cout << "Number: "; //Palabra que aparece al iniciar el programa
+    cin >> number; //Para poder escribir usamos "Cin", number es la variable del principio donde se va a almacenar el número
+    cout << number+1; //Aparece en pantalla el sucesor del numero escrito (Al número le sumamos uno y ya tenemos el sucesor)
+}
